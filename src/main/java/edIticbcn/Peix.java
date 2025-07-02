36324c8ac3c94ad8ebdba7bf989d3c90c442922f
peregiticbcn <pere.guitart@iticbcn.cat>
2025-07-02 18:42:45 +0200

    Introducció i primeres indicacions

--- src/main/java/edIticbcn/Peix.java
+++ src/main/java/edIticbcn/Peix.java
@@ -1,9 +1,14 @@
 package edIticbcn;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  * Exercici 1
  */
 public class Peix {
     private String nom;
+    private int preu;
+    private LocalDate data;
 
 }
